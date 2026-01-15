864b0170917b082737a275dee103dce7b392c4b1
pvshkng <p.kungsapattarakul@gmail.com>
2026-01-15 23:15:56 +0700

    removed log

--- lib/helper.py
+++ lib/helper.py
@@ -3,9 +3,6 @@ from fastmcp import Context
 def get_user(ctx: Context) -> str | None:
     if ctx.request_context:
         context = ctx.request_context
-        print(f"Context: {context}")
-        print(f"request: {context.request}")
-
         user = context.request.query_params.get("user")
 
         return user if user else None
