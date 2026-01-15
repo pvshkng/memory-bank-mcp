e5ee1dcb41582ac30dd66638659d88d63a0b8394
pvshkng <p.kungsapattarakul@gmail.com>
2026-01-15 23:21:04 +0700

    handled ctx unavailable

--- lib/helper.py
+++ lib/helper.py
@@ -1,8 +1,13 @@
 from fastmcp import Context
+from fastmcp.server.dependencies import get_http_request
 
 def get_user(ctx: Context) -> str | None:
+    user = None
     if ctx.request_context:
         context = ctx.request_context
-        user = context.request.query_params.get("user")
+        user = context.request.query_params.get("user")        
+    else:
+        request = get_http_request()
+        user = request.query_params.get("user")
+    return user
 
-        return user if user else None
